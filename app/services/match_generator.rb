7e4677d540ecec99dd5760a76ebb21e1c73ec4bc
nursultan91 <nurs567@yandex.ru>
2018-10-08 13:05:16 +0600

    [Ref] match gen in teams

--- app/services/match_generator.rb
+++ app/services/match_generator.rb
@@ -2,8 +2,9 @@ class MatchGenerator < BaseService
   def call(team1:, team2:, group:)
     Match.transaction do
       match = Match.new(group: group)
-      match.team_scores.new(team: team1, score: rand(10))
-      match.team_scores.new(team: team2, score: rand(10))
+      game_score = 10.times.map { rand(10) }.uniq.last(2)
+      match.team_scores.new(team: team1, score: game_score.first)
+      match.team_scores.new(team: team2, score: game_score.last)
       match.save!
       match
     end
